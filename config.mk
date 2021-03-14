4752a31b0c1049d7677678d98001f68a035be249
DtHnAme <1904685725@qq.com>
2021-03-14 18:52:38 +0800

    gauguin: Drop fix 64mp

--- config.mk
+++ config.mk
@@ -5,7 +5,6 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/bin,$(TARGET_COPY_OUT_SYSTEM)/bin) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/etc,$(TARGET_COPY_OUT_SYSTEM)/etc) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/framework,$(TARGET_COPY_OUT_SYSTEM)/framework) \
-    $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/lib,$(TARGET_COPY_OUT_SYSTEM)/lib) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/lib64,$(TARGET_COPY_OUT_SYSTEM)/lib64) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/vendor/lib64,$(TARGET_COPY_OUT_VENDOR)/lib64) \
     $(call find-copy-subdir-files,*,vendor/xiaomi/miuicamera/proprietary/system/priv-app/MiuiScanner/lib,$(TARGET_COPY_OUT_SYSTEM)/priv-app/MiuiScanner/lib) \
